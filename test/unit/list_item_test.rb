6620f6db0273b5d2e7a4b95d83d537257ec79a84
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2007-08-27 20:05:25 +0000

    initial unit tests
    git-svn-id: file:///apps/svnrepo/trunk/gtd@78 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- test/unit/list_item_test.rb
+++ test/unit/list_item_test.rb
@@ -1,10 +1,19 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ListItemTest < Test::Unit::TestCase
-  fixtures :list_items
+  fixtures :list_items, :users
+def test_crud
+  dmitry = users(:dmitry)
+  doctorsAppointement = list_items(:doctors_appointement);
+  assert doctorsAppointement.save
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  doctorsAppointement.description = "dentist appointement"
+  assert doctorsAppointement.save
+  doctorsAppointement.reload
+
+  assert_equal "dentist appointement", doctorsAppointement.description
+
+  assert doctorsAppointement.destroy
+  
+end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,8 +3,16 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UserTest < Test::Unit::TestCase
   fixtures :users
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  def test_crud
+      dmitry = users(:dmitry)
+      assert dmitry.save
+
+      dmitry.password = "dmitry1234"
+      assert dmitry.save
+      dmitry.reload
+
+      assert_equal "dmitry1234", dmitry.password
+
+      assert dmitry.destroy
+    end
 end
