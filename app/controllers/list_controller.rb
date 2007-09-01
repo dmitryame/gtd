15e1d9de9558a2f7ae462b671b4b8b11b1d81e91
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2007-09-01 04:56:01 +0000

    added rjs select active list
    git-svn-id: file:///apps/svnrepo/trunk/gtd@97 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,11 @@
 class ListController < ApplicationController
   def list
   end
+
+  def activate_list
+    @listType = ListType.find(params[:id])
+    (session[:listType] ||= Hash.new)[@listType.id] = @listType.id
+  end
+
+
 end
