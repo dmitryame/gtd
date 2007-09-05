79a1fdae1f26cb9e7ad813bb8decf9531e89cb12
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2007-09-05 02:50:04 +0000

    ordering lists
    git-svn-id: file:///apps/svnrepo/trunk/gtd@102 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,14 +3,19 @@ class ListController < ApplicationController
   def list
     @listItems         = ListItem.find_all_by_user_id_and_list_type_id(
     session[:user_id], 
-    session[:list_type_id], 
-    :order             => "position DESC") 
+    session[:list_type_id],
+    :order => "position") 
 
     session[:list_type_id] = ListType.find(:first).id if session[:list_type_id] == nil
+    session[:list_items] = @listItems
   end
 
   def activate_list
     session[:list_type_id] = ListType.find(params[:id]).id
+    @listItems             = ListItem.find_all_by_user_id_and_list_type_id(
+    session[:user_id], 
+    session[:list_type_id],
+    :order => "position") 
   end
 
   def add_new_item
@@ -19,15 +24,29 @@ class ListController < ApplicationController
        
     maxSortOrder  = 0 if(maxSortOrder == nil) 
     maxSortOrder  = maxSortOrder + 1
-
     
     newListItem   = ListItem.new(
+    :description  => 'Click To Change Description',
     :user_id      => session[:user_id],
     :list_type_id => session[:list_type_id],
     :position   => maxSortOrder,
     :done         => false
     )
     newListItem.save
+    
+     @listItems             = ListItem.find_all_by_user_id_and_list_type_id(
+      session[:user_id], 
+      session[:list_type_id],
+      :order => "position")
   end
 
+  def sort
+    @listItems  = session[:list_items]        
+    @listItems.each do |list_item|
+      list_item.position = params['all_list_items'].index(list_item.id.to_s) + 1
+      list_item.save
+    end
+    render :nothing      => true
+
+  end
 end
--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -2,7 +2,7 @@ class ListItem < ActiveRecord::Base
   belongs_to :list_type
   belongs_to :user
   acts_as_list :scope=>:user
-  has_many  :action_items, :order => :position
+  has_many  :action_items,   :order => :position
   
   validates_presence_of :user_id, :list_type_id, :position
   
