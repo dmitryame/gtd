49e4de39c3d73c7cf453e4fc9746330d04dec834
dmitry <dmitry@cf6b0be6-772c-0410-b1f0-8363c93e4494>
2007-09-05 03:10:30 +0000

    fixed a bug, storing list items in session
    git-svn-id: file:///apps/svnrepo/trunk/gtd@103 cf6b0be6-772c-0410-b1f0-8363c93e4494

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -7,7 +7,8 @@ class ListController < ApplicationController
     :order => "position") 
 
     session[:list_type_id] = ListType.find(:first).id if session[:list_type_id] == nil
-    session[:list_items] = @listItems
+    session[:list_items] = @listItems        
+    
   end
 
   def activate_list
@@ -16,37 +17,43 @@ class ListController < ApplicationController
     session[:user_id], 
     session[:list_type_id],
     :order => "position") 
+
+    session[:list_items] = @listItems        
+
   end
 
   def add_new_item
     maxSortOrder  = ListItem.maximum(:position,  :conditions   => ["user_id = :user_id and list_type_id = :list_type_id", 
       {:user_id => session[:user_id], :list_type_id => session[:list_type_id]}])
-       
-    maxSortOrder  = 0 if(maxSortOrder == nil) 
-    maxSortOrder  = maxSortOrder + 1
-    
-    newListItem   = ListItem.new(
-    :description  => 'Click To Change Description',
-    :user_id      => session[:user_id],
-    :list_type_id => session[:list_type_id],
-    :position   => maxSortOrder,
-    :done         => false
-    )
-    newListItem.save
-    
-     @listItems             = ListItem.find_all_by_user_id_and_list_type_id(
+
+      maxSortOrder  = 0 if(maxSortOrder == nil) 
+      maxSortOrder  = maxSortOrder + 1
+
+      newListItem   = ListItem.new(
+      :description  => 'Click To Change Description',
+      :user_id      => session[:user_id],
+      :list_type_id => session[:list_type_id],
+      :position   => maxSortOrder,
+      :done         => false
+      )
+      newListItem.save
+
+      @listItems             = ListItem.find_all_by_user_id_and_list_type_id(
       session[:user_id], 
       session[:list_type_id],
       :order => "position")
-  end
-
-  def sort
-    @listItems  = session[:list_items]        
-    @listItems.each do |list_item|
-      list_item.position = params['all_list_items'].index(list_item.id.to_s) + 1
-      list_item.save
+      
+      session[:list_items] = @listItems        
+      
     end
-    render :nothing      => true
 
+    def sort
+      @listItems  = session[:list_items]        
+      @listItems.each do |list_item|
+        list_item.position = params['all_list_items'].index(list_item.id.to_s) + 1
+        list_item.save
+      end
+      render :nothing      => true
+
+    end
   end
-end
