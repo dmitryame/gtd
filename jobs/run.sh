6cae6624dd026ed506a61968dcea73169fa27665
dmitryame <dmitryme@gmail.com>
2008-10-02 14:03:16 -0400

    path adjusted

--- jobs/run.sh
+++ jobs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export RAILS_ENV=production
 export PATH=/usr/local/bin:/usr/bin:/bin:$PATH
-cd /home/getitdon/gtd/jobs
+cd /u/apps/gtd/current/jobs
 ruby ./scheduled_job.rb >>log.log
 
